fc8b16d48cc85ff99dd6cfc9b28c9bee2d0f3a68
Jonathan Borzilleri <jonathan@borzilleri.net>
2017-06-01 21:02:24 -0700

    Initial Commit

---
+++ util.py
@@ -0,0 +1,22 @@
+from datetime import date, datetime
+from dateutil import parser
+
+def json_serialize(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    if isinstance(obj, (datetime, date)):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+def json_deserialize(pairs):
+    """Load with dates"""
+    d = {}
+    for k, v in pairs:
+        if isinstance(v, basestring):
+            try:
+                d[k] = parser.parse(v)
+            except ValueError:
+                d[k] = v
+        else:
+            d[k] = v
+    return d
